d8f1b541a8eab70028569d701595d35a5b3910ec
sharik1040 <nastia.dudchenko2507@gmail.com>
2023-03-15 09:51:15 +0200

    Updated main screen

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Login Register Logout Forms
         </a>
       </header>
     </div>

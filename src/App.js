61bf450b0a52f6a3e0caa68416f95bd750e0e1e7
sharik1040 <nastia.dudchenko2507@gmail.com>
2023-03-15 19:08:34 +0200

    Added LoginForm

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import { LoginForm } from "./components/forms/LoginForm";
+
 function App() {
   return (
     <main className="App">
-      Login Register Logout Forms
+      <LoginForm />
     </main>
   );
 }
---
+++ src/components/forms/LoginForm/index.js
@@ -0,0 +1,75 @@
+import React, { useRef, useState, useEffect, useContext } from "react";
+import AuthContext from "../../../context/AuthProvider";
+
+export const LoginForm = () => {
+    const {auth, setAuth } = useContext(AuthContext);
+    const userRef = useRef();
+    const errRef = useRef();
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errMsg, setErrMsg] = useState("");
+    const [success, setSuccess] = useState(false);
+
+    useEffect(() => {
+        userRef.current.focus();
+    }, [])
+
+    useEffect(() => {
+        setErrMsg("");
+    }, [username, password])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if(!errMsg) setAuth({username, password});
+        setUsername("");
+        setPassword("");
+        setSuccess(!success);
+    }
+
+    return (
+        <>
+            {
+                success ? (
+                    <section>
+                        <h2>Welcome back, {auth.username.toUpperCase()}.</h2>
+                        <br/>
+                        <h3>I hope you are doing well and are safe.</h3>
+                    </section>
+                ) : (
+                    <section>
+                        <form onSubmit={handleSubmit}>
+                            <p ref={errRef} arial-live="assertive">
+                                {errMsg}
+                            </p>
+
+                            <label htmlFor="username">Username:</label>
+                            <input
+                                type="text"
+                                id="username"
+                                ref={userRef}
+                                autoComplete="off"
+                                onChange={(e) => setUsername(e.target.value)}
+                                value={username}
+                                required
+                            />
+                            
+                            <label htmlFor="password">Password:</label>
+                            <input 
+                                type="password"
+                                id="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                                value={password}
+                                required
+                            />
+
+                            <button>Login</button>
+                        </form>
+                        <p>Don't have an account? Register <a href="#">here</a></p>
+                    </section>
+                )
+            }   
+        </>
+    )
+}
+

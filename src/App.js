5269276925cc42efef54c8125a3d1a1545bdbcfd
sharik1040 <nastia.dudchenko2507@gmail.com>
2023-03-15 15:47:16 +0200

    Removed unnecessary files

--- src/App.js
+++ src/App.js
@@ -1,16 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Login Register Logout Forms
-        </p>
-      </header>
-    </div>
+    <main className="App">
+      Login Register Logout Forms
+    </main>
   );
 }
 

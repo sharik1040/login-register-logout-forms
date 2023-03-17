094be3c7cb6448cced0629d24b365a62c5443d73
sharik1040 <nastia.dudchenko2507@gmail.com>
2023-03-17 11:29:35 +0200

    Added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-// import { MaterialUIFormExample } from "./components/MaterialUIForm/MaterialUIFormExample";
+import { Routes, Route } from "react-router-dom";
+import { MaterialUIFormExample } from "./components/MaterialUIForm/MaterialUIFormExample";
 import { VanillaLoginFormExample } from "./components/VanillaLoginForm/VanillaLoginFormExample";
 
 function App() {
   return (
-    <main className="App">
-      {/* <MaterialUIFormExample /> */}
-      <VanillaLoginFormExample />
-    </main>
+    <>
+      <Routes>
+        <Route path="/vanilla-css" element={<VanillaLoginFormExample />}/>
+        <Route path="/material-ui" element={<MaterialUIFormExample />}/>
+      </Routes>
+    </>
   );
 }
 
